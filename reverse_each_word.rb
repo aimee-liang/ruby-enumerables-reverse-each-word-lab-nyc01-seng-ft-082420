409f2948f77b237789354bbab64c2b94bd2cb595
Aimee Liang <aimeeeliang@gmail.com>
2020-08-12 22:44:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(message)
   original_message = message.split("")
   reversed_message = []
   original_message.each do |message|
-    reversed_message << 
+    reversed_message << message.reverse
   return reversed_message.join("")
 end
